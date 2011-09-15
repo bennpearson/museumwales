206517e1dbd9a99902f6e99d8935ea8a6bd8ba32
Benn Pearson <bennpearson@gmail.com>
2011-09-15 15:18:20 +0100

    Added separate folders for Benn Ed and Rob

--- Resources/app.js
+++ Resources/app.js
@@ -8,19 +8,19 @@ var tabGroup = Titanium.UI.createTabGroup();
 //
 // create base UI tab and root window
 //
-var win1 = Titanium.UI.createWindow({  
-    title:'Tab 1',
+var win1 = Titanium.UI.createWindow({   
+	url:'/main_windows/benn/benn.js',
+    title:'Benn',
     backgroundColor:'#fff'
 });
 var tab1 = Titanium.UI.createTab({  
     icon:'KS_nav_views.png',
-    title:'Tab 1',
+    title:'Benn',
     window:win1
 });
 
 var label1 = Titanium.UI.createLabel({
 	color:'#999',
-	text:'I am Window 1',
 	font:{fontSize:20,fontFamily:'Helvetica Neue'},
 	textAlign:'center',
 	width:'auto'
@@ -32,18 +32,18 @@ win1.add(label1);
 // create controls tab and root window
 //
 var win2 = Titanium.UI.createWindow({  
-    title:'Tab 2',
+	url:'/main_windows/ed/ed.js',
+    title:'Ed',
     backgroundColor:'#fff'
 });
 var tab2 = Titanium.UI.createTab({  
     icon:'KS_nav_ui.png',
-    title:'Tab 2',
+    title:'Ed',
     window:win2
 });
 
 var label2 = Titanium.UI.createLabel({
 	color:'#999',
-	text:'I am Window 2',
 	font:{fontSize:20,fontFamily:'Helvetica Neue'},
 	textAlign:'center',
 	width:'auto'
@@ -51,12 +51,35 @@ var label2 = Titanium.UI.createLabel({
 
 win2.add(label2);
 
+//
+// create controls tab and root window
+//
+var win3 = Titanium.UI.createWindow({  
+	url:'/main_windows/rob/rob.js',
+    title:'Rob',
+    backgroundColor:'#fff'
+});
+var tab3 = Titanium.UI.createTab({  
+    icon:'KS_nav_ui.png',
+    title:'Rob',
+    window:win3
+});
+
+var label3 = Titanium.UI.createLabel({
+	color:'#999',
+	font:{fontSize:20,fontFamily:'Helvetica Neue'},
+	textAlign:'center',
+	width:'auto'
+});
+
+win3.add(label3);
 
 //
 //  add tabs
 //
 tabGroup.addTab(tab1);  
 tabGroup.addTab(tab2);  
+tabGroup.addTab(tab3);
 
 
 // open tab group
