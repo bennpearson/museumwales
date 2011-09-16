1713dc5450521c149742328e14bca9aaf12ef749
Robert Gapper <icanseethroughtime@hotmail.com>
2011-09-16 16:38:32 +0100

    added image to left of all rows... and BG image to full screen

--- Resources/main_windows/buildings/buildings.js
+++ Resources/main_windows/buildings/buildings.js
@@ -1,6 +1,6 @@
 // Hi Rob, here is your file and folder, add ui.js or whatever you want to your folder
 
-var win = Titanium.UI.currentWindow;
+//var win = Titanium.UI.currentWindow;
 
 
 // var testLabel = Titanium.UI.createLabel({
@@ -14,15 +14,55 @@ var win = Titanium.UI.currentWindow;
     // height:'auto'
 // });
 
-var StFagonsData = [
-{ title:"The Galleries"},
-{ title:"Kennixton Farmhouse"},
-{ title:"Bee Shelter"},
-{ title:"Circular Pigsty"}
+// var StFagonsData = [
+// { title:"The Galleries"},
+// { title:"Kennixton Farmhouse"},
+// { title:"Bee Shelter"},
+// { title:"Circular Pigsty"}
+// ];
+	// var TheTable = Titanium.UI.createTableView({
+	// data:StFagonsData
+// });
+
+var win = Titanium.UI.currentWindow;
+win.setBackgroundImage('images/paper.jpg');
+
+var number = Ti.UI.currentWindow.number;
+
+// create table view data object
+
+var data = [
+{leftImage:'images/house1.jpg', title:'Metal Works', hasChild:true, header:'Buildings', urlink:'metalworks.js'},
+{leftImage:'images/house1.jpg', title:'Tollgate', hasChild:true, urlink:'tollgate.js'},
+{leftImage:'images/house1.jpg', title:'Shop', hasChild:true, urlink:'shop.js'}
 ];
-	var TheTable = Titanium.UI.createTableView({
-	data:StFagonsData
+
+// create auto' view
+var tableview = Titanium.UI.createTableView({
+     data:data,
+    style:Titanium.UI.iPhone.TableViewStyle.GROUPED
 });
 
 
-win.add(StFagonsData);
+// create table view event listener
+tableview.addEventListener('click', function(e)
+{
+    if (e.rowData.urlink)
+    {
+         var win = Titanium.UI.createWindow({
+         	backgroundColor:'transparent',
+         	rowBackgroundColor:'#000',
+            url:e.rowData.urlink,
+            title:e.rowData.title
+        });
+        Titanium.UI.currentTab.open(win,{animated:true});
+    }
+});
+
+// add table view to the window
+ Titanium.UI.currentWindow.add(tableview);
+
+
+
+
+// win.add(StFagonsData);
