805111ea57310d7c4a735dbad3b39e6eef8de250
Benn Pearson <bennpearson@gmail.com>
2011-09-21 10:16:06 +0100

    branched off
    I've branched off because my barcode will contain a module which is
    stored outside this project in /Library/Application\ Support/Titanium
    folder and will cause the app to crash.

--- Resources/main_windows/barcode/barcode.js
+++ Resources/main_windows/barcode/barcode.js
@@ -4,7 +4,7 @@ var win = Titanium.UI.currentWindow;
 
 
 var testLabel = Titanium.UI.createLabel({
-    text:'Hello Benn',
+    text:'Hello Benn Edit',
     color:'#000',
     textAlign:'left',
     font:{fontSize:14},
