53145dcd93b3b668e463ab7922beb24fee4a16ca
Ed Millington <e.r.millingtondunelm.org.uk>
2011-09-24 22:07:09 +0100

    I've made the following changes to the map (see extended description):
    1) updated how the map will actually look (an edited version of the map
    from the tourist information leaflet)
    2) updated how the 'you are here' pointer will actually look (a bright
    red monty python hand because; why the hell not? Currently, it still
    comes and finds you when you click on the map)
    3) added a 'help' button on the top left (which opens a currently-blank
    overlaid window)
    4) the building in the very top left (Y Garreg Fawr Farmhouse) is
    'clickable' (again, opening a currently-blank overlaid window)
    5) the map begins zoomed and centred on to the visitor centre

--- Resources/main_windows/map/map.js
+++ Resources/main_windows/map/map.js
@@ -1,49 +1,147 @@
-// Hi Rob, here is your file and folder, add ui.js or whatever you want to your folder
+
+
 
 var win = Titanium.UI.currentWindow;
 
 
+
+// the scrollview starts here
+
 var scrollView = Titanium.UI.createScrollView({
   contentHeight: 'auto',
-  contentWidth: '525',
-  maxZoomScale: 4,
-  minZoomScale: 1,
+  contentWidth: '2500',
+  maxZoomScale: 1,
+  minZoomScale: 0.3,
   showHorizontalScrollIndicator: true,
   showVerticalScrollIndicator: true,
   top: 0,
   disableBounce: true,
-  backgroundColor: '#ffffcc',
+  backgroundImage: 'images/background.png',
+  zoomScale: 0.5,
 });
 
-image = Titanium.UI.createImageView({
-  image: 'images/mapbg.jpg',
+  scrollView.setContentOffset({x:500 ,y:225});
+
+// the scrollview ends here
+
+
+
+// the help window starts here
+
+var helpbutton = Titanium.UI.createImageView({
+	height: 40,
+	width: 40,
+	top: 5,
+	left: 5,
+	image: 'images/red-question-mark.png',
+	
 });
 
-image2 = Titanium.UI.createImageView({
-  backgroundColor: 'blue',
-  width:10,
-  height:10,
-  left:72,
+helpbutton.addEventListener('singletap', function(e)
+
+
+{
+	var tt = Titanium.UI.create2DMatrix();
+	tt = tt.scale(0);
+
+
+    var help = Titanium.UI.createWindow({
+		backgroundColor:'#336699',
+		height:358,
+		width:310,
+		top:49,
+		opacity:0.92,
+		transform:tt
+	});
+
+	// create first transform to go beyond normal size
+	var tt1 = Titanium.UI.create2DMatrix();
+	tt1 = tt1.scale(1.1);
+	var aa = Titanium.UI.createAnimation();
+	aa.transform = tt1;
+	aa.duration = 400;
+
+	// when this animation completes, scale to normal size
+	aa.addEventListener('complete', function()
+	{
+		Titanium.API.info('here in complete');
+		var tt2 = Titanium.UI.create2DMatrix();
+		tt2 = tt2.scale(1.0);
+		help.animate({transform:tt2, duration:200});
+
+	});
+
+	// create a button to close window
+	var helpclose = Titanium.UI.createButton({
+		title:'Close',
+		height:30,
+		width:150
+	});
+	help.add(helpclose);
+	helpclose.addEventListener('click', function()
+	{
+		var tt3 = Titanium.UI.create2DMatrix();
+		tt3 = tt3.scale(0);
+		help.close({transform:tt3,duration:300});
+	});
+
+	help.open(aa);
+
+});   
+
+// the help window ends here
+
+
+
+// the map starts here
+
+map = Titanium.UI.createImageView({
+  image: 'images/mapbg2.jpg',
+  height: 1415,
+  top: 0,
+});
+
+// the map ends here
+
+
+
+// the animation for all the building windows starts here
+
+	var t = Titanium.UI.create2DMatrix();
+	t = t.scale(0);
+	
+	// create first transform to go beyond normal size
+	var t1 = Titanium.UI.create2DMatrix();
+	t1 = t1.scale(1.1);
+	var a = Titanium.UI.createAnimation();
+	a.transform = t1;
+	a.duration = 200;
+
+	// when this animation completes, scale to normal size
+	Titanium.API.info('here in complete');
+	var t2 = Titanium.UI.create2DMatrix();
+	t2 = t2.scale(1.0);
+
+	var t3 = Titanium.UI.create2DMatrix();
+	t3 = t3.scale(0);
+
+// the animation for all the building windows ends here
+
+
+
+// building33 (Y Garreg Fawr Farmhouse) starts here
+
+bimg33 = Titanium.UI.createImageView({
+  width:80,
+  height:90,
+  left:200,
   top: 110
 });
 
-image2.addEventListener('click', function(event){
-	if(image2.backgroundColor == 'blue') {
-		image2.backgroundColor = 'green';
-	} 
-	else {
-		image2.backgroundColor = 'blue';
-	}
-	});
-
-
-
-image2.addEventListener('click', function()
+bimg33.addEventListener('click', function()
 {
-	var t = Titanium.UI.create2DMatrix();
-	t = t.scale(0);
 
-	var w = Titanium.UI.createWindow   ({
+	var bwin33 = Titanium.UI.createWindow   ({
 		backgroundColor:'#336699',
 		borderWidth:8,
 		borderColor:'#999',
@@ -55,73 +153,68 @@ image2.addEventListener('click', function()
 		transform:t
 	});
 
-	// create first transform to go beyond normal size
-	var t1 = Titanium.UI.create2DMatrix();
-	t1 = t1.scale(1.1);
-	var a = Titanium.UI.createAnimation();
-	a.transform = t1;
-	a.duration = 200;
 
-	// when this animation completes, scale to normal size
 	a.addEventListener('complete', function()
 	{
-		Titanium.API.info('here in complete');
-		var t2 = Titanium.UI.create2DMatrix();
-		t2 = t2.scale(1.0);
-		w.animate({transform:t2, duration:200});
+		bwin33.animate({transform:t2, duration:200});
 
 	});
 
 	// create a button to close window
-	var b = Titanium.UI.createButton({
+	var b33 = Titanium.UI.createButton({
 		title:'Close',
 		height:30,
 		width:150
 	});
-	w.add(b);
-	b.addEventListener('click', function()
+	
+	bwin33.add(b33);
+	
+	b33.addEventListener('click', function()
 	{
-		var t3 = Titanium.UI.create2DMatrix();
-		t3 = t3.scale(0);
-		w.close({transform:t3,duration:300});
+		bwin33.close({transform:t3,duration:300});
 	});
 
-	w.open(a);
-
+	bwin33.open(a);
 
 });
 
-image3 = Titanium.UI.createImageView({
-  backgroundColor: 'blue',
-  width:5,
-  height:30,
-  left:270,
-  top: 150,
+// building33 (Y Garreg Fawr Farmhouse) ends here
+
+
+
+// the hand starts here
+
+hand = Titanium.UI.createImageView({
+  image: 'images/redhand.png',
+  width:90,
+  height:100,
+  left:1300,
+  top: 750,
 });
 
-
-image.addEventListener('singletap', function(e)
+map.addEventListener('singletap', function(e)
 {
-   image.add(image3);
+   map.add(hand);
 
    
 var a = Ti.UI.createAnimation();
-a.left = (e.x-2.5);
-a.top = (e.y-30);
+a.left = (e.x);
+a.top = (e.y-100);
 a.duration = 200;
    
-   image3.animate(a, function(){
+   hand.animate(a, function(){
    	
    });
+   
 
 });
 	
+// the hand ends here
 
 
-
-scrollView.add(image);
-scrollView.add(image2);
+scrollView.add(map);
+scrollView.add(bimg33);
 
 win.add(scrollView);
-
+win.add(helpbutton);
 
