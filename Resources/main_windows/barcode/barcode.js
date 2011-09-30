e926e8aa067af9a144fbc5551bf9b180432ea3db
Benn Pearson <bennpearson@gmail.com>
2011-09-30 14:38:43 +0100

    Added barcode reader
    tibar module needed in directory
    /Library/Application Support/Titanium/modules/iphone/tibar/0.4.2
    
    tiapp.xml module linked to correct version
    <modules>
       <module version="0.4.2">tibar</module>
    </modules>
    
    Below code needed to be added to module.xconfig
    OTHER_LDFLAGS=$(inherited) -framework AVFoundation -framework CoreMedia
    -framework CoreVideo -framework QuartzCore /usr/lib/libiconv.dylib
    
    Ref:
    http://code.google.com/p/tibar/
    http://code.google.com/p/tibar/issues/detail?id=16#c14

--- Resources/main_windows/barcode/barcode.js
+++ Resources/main_windows/barcode/barcode.js
@@ -1,17 +1,52 @@
-// Hi Me, here is your file and folder, add ui.js or whatever you want to your folder
-
-var win = Titanium.UI.currentWindow;
-
-
-var testLabel = Titanium.UI.createLabel({
-    text:'Hello Benn Edit two',
-    color:'#000',
-    textAlign:'left',
-    font:{fontSize:14},
-    top:10,
-    left:10,
-    width:300,
-    height:'auto'
+var win = Titanium.UI.createWindow({
+    title:'TiBar Test App',
+    backgroundColor:'#fff'
 });
 
-win.add(testLabel);
+var TiBar = require('tibar');
+var label = Titanium.UI.createLabel({
+    text:'TiBar App',
+    textAlign:'center',
+    width:'auto'
+});
+
+var button = Ti.UI.createButton({
+    title: "Scan barcode",
+    height:50,
+    width:250,
+    bottom:20
+});
+
+button.addEventListener('click', function(){
+    TiBar.scan({
+        // simple configuration for iPhone simulator
+        configure: {
+            classType: "ZBarReaderController",
+            sourceType: "Album",
+            cameraMode: "Default",
+            symbol:{
+                "QR-Code":true,
+            }
+        },
+        success:function(data){
+            Ti.API.info('TiBar success callback!');
+            if(data && data.barcode){
+                Ti.UI.createAlertDialog({
+                    title: "Scan result",
+                    message: "Barcode: " + data.barcode + " Symbology:" + data.symbology
+                }).show();
+            }
+        },
+        cancel:function(){
+            Ti.API.info('TiBar cancel callback!');
+        },
+        error:function(){
+            Ti.API.info('TiBar error callback!');
+        }
+    });        
+});
+
+win.add(label);
+win.add(button);
+
+win.open();
